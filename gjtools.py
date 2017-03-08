d0e10485386ad9c04f859eb7a293047d0d4f3995
Jeffrey Elkner <jeff@elkner.net>
2017-03-08 13:27:53 -0500

    Added reduced_row_echelon to gjtools.py with test in gje2.py

--- gjtools.py
+++ gjtools.py
@@ -1,3 +1,5 @@
+from fractions import Fraction
+
 def print_matrix(m):
     for row in m:
         print('|{0:6s}{1:6s}{2:6s}| {3:6s}|'.format(*[str(val) for val in row]))
@@ -12,3 +14,11 @@ def mul_row(m, k, ri):
 def add_row(m, k, ri, rj):
     for i in range(len(m[ri-1])):
         m[rj-1][i] += k * m[ri-1][i]
+
+
+def reduced_row_echelon(m):
+    """Transform matrix m into reduced row echelon form"""
+    # Multiply the first row by the multiplicative inverse of the first element
+    recip = Fraction(1, m[0][0])
+    for i in range(len(m[0])):
+        m[0][i] *= recip
