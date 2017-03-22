c79b09ee1d8e6b875caae75c6bbafabacc165c8f
Marco Sirabella <msirabel@gmail.com>
2017-03-22 18:01:49 -0400

    - More pythonic

--- src/gaussjordan/examples/__init__.py
+++ src/gaussjordan/examples/__init__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+__all__ = ['example{}'.format(num) for num in range(1, 4 + 1)]
 __appname__     = ""
 __author__      = "Jeffrey Elkner"
 __copyright__   = ""
 __credits__     = ["Jeffrey Elkner"]  # Authors and bug reporters
-
-from . import *
