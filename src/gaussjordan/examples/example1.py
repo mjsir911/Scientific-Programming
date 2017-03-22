1b9ac79939670ace8400b68ec49fca0b7d290f08
Marco Sirabella <msirabel@gmail.com>
2017-03-22 18:01:56 -0400

    - Abandon all hope to recover

--- src/gaussjordan/examples/example1.py
+++ src/gaussjordan/examples/example1.py
@@ -2,16 +2,16 @@ from .. import tools
 from fractions import Fraction
 
 m = [[1, -2, 3, 9], [-1, 3, 0, -4], [2, -5, 5, 17]]
-print_matrix(m)
-mul_row(m, Fraction(1, 2), 3)
-print_matrix(m)
+tools.print_matrix(m)
+tools.Reduced_Echelon.mul(m, 2, Fraction(1, 2))
+tools.print_matrix(m)
 add_row(m, 1, 1, 2)
 add_row(m, -1, 1, 3)
-print_matrix(m)
+tools.print_matrix(m)
 add_row(m, Fraction(1, 2), 2, 3)
-print_matrix(m)
+tools.print_matrix(m)
 add_row(m, -3, 3, 2)
 add_row(m, -3, 3, 1)
-print_matrix(m)
+tools.print_matrix(m)
 add_row(m, 2, 2, 1)
-print_matrix(m)
+tools.print_matrix(m)
