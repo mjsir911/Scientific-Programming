e349fbb47b525743d0e3886aae018b53b17f8566
Marco Sirabella <msirabel@gmail.com>
2017-05-01 11:53:34 -0400

    - More physics stuff

---
+++ src/electricity/gravity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import scipy.constants
+
+__appname__     = ""
+__author__      = "Marco Sirabella"
+__copyright__   = ""
+__credits__     = ["Marco Sirabella"]  # Authors and bug reporters
+__license__     = "GPL"
+__version__     = "1.0"
+__maintainers__ = "Marco Sirabella"
+__email__       = "msirabel@gmail.com"
+__status__      = "Prototype"  # "Prototype", "Development" or "Production"
+__module__      = ""
+
+
+def gfs(mass, radius):
+    (scipy.constants.gravitational_constant * m) / r ** 2
