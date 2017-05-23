a91e388f61c90a205e3b14a95f3b6abb19c7aa7d
Ali Abbas <aabbas@ebgranville.gctaa.net>
2017-05-23 12:08:59 -0400

    Fixed variable length polys

--- polynomial.py
+++ polynomial.py
@@ -4,22 +4,24 @@ def print_poly(p):
       '4x^2 + 3x + 2'
       >>> print_poly([2, 8, 3])
       '2x^2 + 8x + 3'
+      >>> print_poly([8, 6, 7, 9])
+      '8x^3 + 6x^2 + 7x + 9'
     """
-    """
-      >>> print_poly([6, 0, 5])
-      6x^2 + 5
-      >>> print_poly([7, 0, -3, 5])
-      7x^3 - 3x + 5
-      >>> print_poly([1, -1, 0, 0, -3, 2])
-      x^5 - x^4 - 3x + 2
-      >>> print_poly([3, 0, 0, 5])
-      3x^3 + 5
-      >>> print_poly([7, 2, 5, 0, 3])
-      7x^4 + 2x^3 + 5x^2 + 3
-      >>> print_poly([-5, 1, -3, -2, 9])
-      -5x^4 + x^3 - 3x^2 - 2x + 9
-      >>> print_poly([1, 8, -4, 0, 0 , 5, -8])
-      x^6 + 8x^5 - 4x^4 + 5x - 8
-      >>> print_poly([-5, 3, 0, 40])
-      -5x^3 + 3x^2 + 40
-    """
+    polystr = ''    
+    expon = len(p) - 1 
+    
+    for i in range(len(p)):
+        if expon == 0:
+            polystr += str(p[-1])
+        elif expon == 1:
+            polystr += str(p[-2]) + 'x + '
+        else:
+            polystr += '{0}x^{1} + '.format(p[i], expon)
+        expon -= 1
+    
+    return polystr
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
