e4e8e1cd5b9ba918d26044d67d58809267d051ea
Ali Abbas <aabbas@ebgranville.gctaa.net>
2017-05-23 12:29:30 -0400

    made a mess broke thingz

--- polynomial.py
+++ polynomial.py
@@ -6,19 +6,22 @@ def print_poly(p):
       '2x^2 + 8x + 3'
       >>> print_poly([8, 6, 7, 9])
       '8x^3 + 6x^2 + 7x + 9'
+      >>> print_poly([7, -3, 5, -6])
+      '7x^3 - 3x^2 + 5x - 6'
     """
     polystr = ''    
     expon = len(p) - 1 
     
     for i in range(len(p)):
-        if expon == 0:
-            polystr += str(p[-1])
-        elif expon == 1:
-            polystr += str(p[-2]) + 'x + '
-        else:
-            polystr += '{0}x^{1} + '.format(p[i], expon)
+        coeff = str(p[i])
+        if i == len(p) - 1:
+            polystr += coeff
+            break
+
         expon -= 1
     
+        polystr += '{0}{1}{2}'.format(coeff, expon, opp)
+
     return polystr
 
 
