0bcd90349c6485828052e045cda32519e0219bb5
Ali Abbas <aabbas@ebgranville.gctaa.net>
2017-05-25 12:07:36 -0400

    changed lists into tuples

--- polynomial.py
+++ polynomial.py
@@ -1,14 +1,14 @@
 def get_opp(p, i):
     """
-      >>> get_opp([7, -3, 5, -6], 1)
+      >>> get_opp((7, -3, 5, -6), 1)
       ' - '
-      >>> get_opp([7, -3, 5, -6], 2)
+      >>> get_opp((7, -3, 5, -6), 2)
       ' + '
-      >>> get_opp([7, -3, 5, -6], 3)
+      >>> get_opp((7, -3, 5, -6), 3)
       ' - '
-      >>> get_opp([7, -3, 5, -6], 0)
+      >>> get_opp((7, -3, 5, -6), 0)
       ''
-      >>> get_opp([-3, 5, -6], 0)
+      >>> get_opp((-3, 5, -6), 0)
       '-'
     """
     if i == 0:
@@ -23,15 +23,15 @@ def get_opp(p, i):
 
 def get_xterm(p, i):
     """
-      >>> get_xterm([8, 6, 7, 9], 0)
+      >>> get_xterm((8, 6, 7, 9), 0)
       'x^3'
-      >>> get_xterm([1, 8, 6, 7, 9], 2)
+      >>> get_xterm((1, 8, 6, 7, 9), 2)
       'x^2'
-      >>> get_xterm([1, 8, 6, 7, 9], 0)
+      >>> get_xterm((1, 8, 6, 7, 9), 0)
       'x^4'
-      >>> get_xterm([8, 6, 7, 9], 2)
+      >>> get_xterm((8, 6, 7, 9), 2)
       'x'
-      >>> get_xterm([8, 6, 7, 9], 3)
+      >>> get_xterm((8, 6, 7, 9), 3)
       ''
     """
     exp = len(p) - (i + 1)
@@ -44,19 +44,19 @@ def get_xterm(p, i):
 
 def poly_to_str(p):
     """
-      >>> poly_to_str([4, 3, 2])
+      >>> poly_to_str((4, 3, 2))
       '4x^2 + 3x + 2'
-      >>> poly_to_str([2, 8, 3])
+      >>> poly_to_str((2, 8, 3))
       '2x^2 + 8x + 3'
-      >>> poly_to_str([8, 6, 7, 9])
+      >>> poly_to_str((8, 6, 7, 9))
       '8x^3 + 6x^2 + 7x + 9'
-      >>> poly_to_str([7, -3, 5, -6])
+      >>> poly_to_str((7, -3, 5, -6))
       '7x^3 - 3x^2 + 5x - 6'
-      >>> poly_to_str([5, 0, 2])
+      >>> poly_to_str((5, 0, 2))
       '5x^2 + 2'
-      >>> poly_to_str([-7, 0, 3, 5, 0, -2])
+      >>> poly_to_str((-7, 0, 3, 5, 0, -2))
       '-7x^5 + 3x^3 + 5x^2 - 2'
-      >>> poly_to_str([-7, 0, 0, 5, 0, 0])
+      >>> poly_to_str((-7, 0, 0, 5, 0, 0))
       '-7x^5 + 5x^2'
     """
     polystr = ''
